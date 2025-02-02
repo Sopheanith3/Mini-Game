6250f609d204befe69cb24604b1dbec4e86011ca
Sopheanith3 <nsopheanith3@gmail.com>
2025-02-01 23:55:23 -0800

    adding the assets and game, main, player bird for the flappy bird

--- game.js
+++ game.js
@@ -4,36 +4,135 @@ class Game {
         this.canvas.width = 1400;
         this.canvas.height = 800;
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(this.update.bind(this),6);
         this.obstacles = [];
-        this.initEventListiners();
+        this.score = new TextComponent("30px", "Consolas", "black", 1200, 50);
+        this.background = new BackgroundComponent(1400, 660, "assets/background.png", 0, 0);
+        this.land = new BackgroundComponent(400, 150, "assets/land.png", 0, 650);
+        this.gameOverImage = new ImageComponent(140, 140, "assets/gameover.png", 600, 300);
+        this.player = new Player(70, 70, "assets/bird1.png", 700, 245);
+        this.isGameOver = false;
     }
-    initEventListiners() {
-        window.addEventListener('keydown', this.handleKeyDown.bind(this));
-        window.addEventListener('keyup', this.handleKeyUp.bind(this));
-        window.addEventListener('mousedown', this.handleKeyDown.bind(this));
-        window.addEventListener('mouseup', this.handleMouseUp.bind(this));
+
+    start() {
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.setupEventListeners();
+        this.interval = setInterval(() => this.updateGameArea(), 6);
     }
-    handleKeyDown() {
-        player.jump();
-        player.accelerate(0.05);
+
+    setupEventListeners() {
+        window.addEventListener('keydown', () => this.player.jump());
+        window.addEventListener('mousedown', () => this.player.jump());
     }
-    handleKeyUp() {
-        player.accelerate(0.05);
-    }
-    handleMouseDown() {
-        player.jump();
-        player.accelerate(0.05);
-    }
-    handleMouseUp() {
-        player.accelerate(0.05);
-    }
-    clear() {
-        this.context.clearReact(0,0, this.canvas.width, this.canvas.height);
-    }
+
     stop() {
         clearInterval(this.interval);
+        this.isGameOver = true;
+    }
+
+    clear() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    updateGameArea() {
+        if (this.checkCollision()) {
+            this.gameOverImage.update(this.context);
+            this.stop();
+            return;
+        }
+
+        this.clear();
+        this.updateBackground();
+        this.frameNo++;
+        this.createObstacles();
+        this.updateObstacles();
+        this.player.updatePosition(this.canvas.height);
+        this.player.update(this.context);
+        this.score.text = "SCORE: " + Math.floor(this.frameNo / 20);
+        this.score.update(this.context);
+    }
+
+    checkCollision() {
+        return this.obstacles.some(obstacle => this.player.crashWith(obstacle));
+    }
+
+    updateBackground() {
+        this.background.speedX = -1;
+        this.background.updatePosition();
+        this.background.update(this.context);
+
+        this.land.speedX = -1;
+        this.land.updatePosition();
+        this.land.update(this.context);
+    }
+
+    createObstacles() {
+        if (this.frameNo === 2 || this.everyInterval(300)) {
+            const x = this.canvas.width;
+            const height = Math.floor(Math.random() * (300 - 40 + 1) + 40);
+            const gap = Math.floor(Math.random() * (300 - 170 + 1) + 170);
+
+            this.obstacles.push(new ImageComponent(60, height, "assets/pp.png", x, 0));
+            this.obstacles.push(new ImageComponent(70, 25, "assets/head.png", x - 5, height));
+            this.obstacles.push(new ImageComponent(70, 25, "assets/head.png", x - 5, height + gap));
+            this.obstacles.push(new ImageComponent(60, this.canvas.height - height - gap - 150 - 25, "assets/pp.png", x, height + gap + 25));
+        }
+    }
+
+    updateObstacles() {
+        this.obstacles.forEach(obstacle => {
+            obstacle.x -= 1;
+            obstacle.update(this.context);
+        });
+    }
+
+    everyInterval(n) {
+        return (this.frameNo / n) % 1 === 0;
+    }
+}
+
+class TextComponent {
+    constructor(fontSize, font, color, x, y) {
+        this.fontSize = fontSize;
+        this.font = font;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.text = "";
+    }
+
+    update(ctx) {
+        ctx.font = `${this.fontSize} ${this.font}`;
+        ctx.fillStyle = this.color;
+        ctx.fillText(this.text, this.x, this.y);
+    }
+}
+
+class ImageComponent {
+    constructor(width, height, imageSrc, x, y) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.image = new Image();
+        this.image.src = imageSrc;
+    }
+
+    update(ctx) {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+}
+
+class BackgroundComponent extends ImageComponent {
+    constructor(width, height, imageSrc, x, y) {
+        super(width, height, imageSrc, x, y);
+        this.speedX = 0;
+    }
+
+    updatePosition() {
+        this.x += this.speedX;
+        if (this.x <= -this.width) {
+            this.x = 0;
+        }
     }
 }
--- player.js
+++ player.js
@@ -1,11 +1,40 @@
-class Player extends GameComponent {
-    constructor() {
-        super(70,70, "./assets/bird1.png", 700, 245, "image");
+class Player {
+    constructor(width, height, imageSrc, x, y) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.gravity = 0.05;
+        this.gravitySpeed = 0;
+        this.jumpStrength = -2;
     }
+
+    update(ctx) {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+
+    updatePosition(canvasHeight) {
+        this.gravitySpeed += this.gravity;
+        this.y += this.gravitySpeed;
+        if (this.y < 0) this.y = 0;
+        if (this.y > canvasHeight - this.height - 140) {
+            this.y = canvasHeight - this.height - 140;
+            this.gravitySpeed = 0;
+        }
+    }
+
     jump() {
-        this.gravitySpeed = -2;
+        this.gravitySpeed = this.jumpStrength;
     }
-    accelerate(speed) {
-        this.gravity = speed;
+
+    crashWith(other) {
+        return !(
+            this.y + this.height < other.y ||
+            this.y > other.y + other.height ||
+            this.x + this.width < other.x ||
+            this.x > other.x + other.width
+        );
     }
 }
